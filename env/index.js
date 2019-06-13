4c3a9dfc4af43f16eff0e61993b8650488db9591
avicha <avichabc@gmail.com>
2019-06-13 21:20:53 +0800

    feat:去掉了common.env

--- env/index.js
+++ env/index.js
@@ -3,19 +3,8 @@ const path = require('path')
 
 const env = process.env
 const NODE_ENV = env.NODE_ENV || 'development'
-const commonEnvFile = path.join(__dirname, 'common.env')
 const envFile = path.join(__dirname, `${NODE_ENV}.env`)
 
-if (fs.existsSync(commonEnvFile)) {
-  const commonEnvContent = fs.readFileSync(commonEnvFile, {
-    encoding: 'utf8'
-  })
-  commonEnvContent.split('\n').forEach(line => {
-    if (line) {
-      env[line.split('=')[0]] = line.split('=')[1]
-    }
-  })
-}
 if (fs.existsSync(envFile)) {
   const envContent = fs.readFileSync(envFile, {
     encoding: 'utf8'
